32b1eebf7758d8563aa984c619ec93406753a6dd
LiteHell <litehell@litehell.info>
2023-07-15 14:59:49 +0900

    Delete webtoon rss feature

--- src/web/index.js
+++ src/web/index.js
@@ -1,7 +1,6 @@
 let express = require('express'),
     config = require('../../data/config.json'),
     path = require('path'),
-    axios = require('axios'),
     app = express(),
     CauBoardFeed = require('../feed/cau/board'),
     redis = require('redis'),
@@ -20,78 +19,6 @@ app.set('views', path.join(__dirname, '../../views'));
 app.set('view engine', 'pug');
 app.use(express.static(path.join(__dirname, '../../static')));
 
-app.get('/webtoon/daum/:viewerId/toWebtoonId', (req, res, next) => {
-    let {viewerId} = req.params;
-    req.cacheKey = `daum/${viewerId}/webtoonId`
-    redisClient.get(req.cacheKey, (err, reply) => {
-        if (err || reply == null)
-            next();
-        else
-            res.type("application/json").end(reply);
-    })
-}, (req, res, next) => {
-    let {viewerId} = req.params;
-    axios.get(`http://webtoon.daum.net/data/pc/webtoon/viewer/${viewerId}`).then(response => {
-        try {
-            let resJson = JSON.stringify(response.data.data.webtoonEpisode.webtoon.nickname || null);
-            res.type("application/json").end(resJson);
-            redisClient.set(req.cacheKey, resJson, 'EX', 300);
-        } catch (err) {
-            res.type("application/json").end('null')
-            next(err);
-        }
-    });
-});
-app.get('/webtoon/:sitename/:webtoonId/:feedtype', (req, res, next) => {
-    let {sitename, webtoonId, feedtype} = req.params
-    req.cacheKey = `${sitename}/${webtoonId}/${feedtype}`;
-    redisClient.get(req.cacheKey, (err, reply) => {
-        if (err || reply == null)
-            next();
-        else
-            switch(feedtype) {
-                case 'rss':
-                    res.type(rssMime).end(reply)
-                    break;
-                case 'atom':
-                    res.type(atomMime).end(reply)
-                    break;
-                default:
-                    next(new Error("Unsupported feed type"));
-            }
-    });
-}, (req, res, next) => {
-    let webtoonClass;
-    switch(req.params.sitename) {
-        case 'naver':
-            webtoonClass = require('../parsers/naver');
-            break;
-        case 'daum':
-            webtoonClass = require('../parsers/daum');
-            break;
-        default:
-            throw new Error("Unsupported webtoon site");
-    }
-    let webtoon = new webtoonClass(req.params.webtoonId)
-    let feed = new (require('../feed/webtoon'))(webtoon);
-    switch(req.params.feedtype) {
-        case 'rss':
-            feed.rss().then(rss => {
-                redisClient.set(req.cacheKey, rss, 'EX', 300);
-                res.type(rssMime).end(rss)
-            }).catch(next)
-            break;
-        case 'atom':
-            feed.atom().then(atom => {
-                redisClient.set(req.cacheKey, atom, 'EX', 300);
-                res.type(atomMime).end(atom)
-            }).catch(next)
-            break;
-        default:
-            throw new Error("Unsupported feed type");
-    }
-
-});
 app.get('/cau/:parserName/:feedtype', (req, res, next) => {
     let {parserName, feedtype} = req.params
     req.cacheKey = `cau/${parserName}/${feedtype}`;
