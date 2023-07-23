ad01e69877607dbffe14de054476bc87afdc2eb5
LiteHell <litehell@litehell.info>
2023-07-23 17:09:18 +0900

    Fix a bug that redirect is done wrong way

--- server/server.go
+++ server/server.go
@@ -25,6 +25,7 @@ func CreateServer() *gin.Engine {
 		ctx.Redirect(308, "https://www.cau.ac.kr/cms/FR_PRO_CON/BoardRss.do?pageNo=1&pagePerCnt=15&MENU_ID=100&SITE_NO=2&BOARD_SEQ=4&S_CATE_SEQ=&BOARD_TYPE=C0301&BOARD_CATEGORY_NO=&P_TAB_NO=&TAB_NO=&P_CATE_SEQ=&CATE_SEQ=&SEARCH_FLD=SUBJECT&SEARCH=")
 	})
 	for from, to := range map[string]string{"sw": "/cau/swedu/", "dormitory": "/cau/dormitory/seoul/all/"} {
+		from, to := from, to
 		server.GET(fmt.Sprintf("/cau/%s/:feedType", from), func(ctx *gin.Context) {
 			ctx.Redirect(308, to+ctx.Param("feedType"))
 		})
